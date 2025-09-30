35e4d29ea32a6e31d1c53cc6733b60223982052e
Enzo Velo <enzo_vel@hotmail.com>
2025-09-30 13:53:30 -0300

    feat: start project

---
+++ src/test/java/com/example/criteria_project/CriteriaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criteria_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
